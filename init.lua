711d8c4bf20db1dc7adbd00aa5bf212bff220edf
Sophistiqq <roi.for.school@gmail.com>
2024-01-06 11:07:08 +0800

    Initial commit of my neovim config

---
+++ init.lua
@@ -0,0 +1,33 @@
+vim.cmd("set tabstop=2")
+vim.cmd("set expandtab")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.g.mapleader = " "
+vim.opt.laststatus = 3
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+vim.cmd("hi linenr guibg=none")
+vim.cmd("hi CursorN guifg=auto")
